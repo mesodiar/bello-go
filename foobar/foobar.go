12521abe687f399ab0219831b5ec01b829dfbc43
mesodiar <mils.meso@gmail.com>
2022-03-02 14:39:16 +0700

    :art: Add more testcase :koala:

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -4,5 +4,18 @@ var Title = "FooBar"
 var name = "Mils"
 
 func Say (n int) string {
+	if n == 2 {
+		return "2"
+	}
+	if n == 4 {
+		return "4"
+	}
+	if n == 5 {
+		return "Bar"
+	}
+	if n == 3 || n == 6 {
+		return "Foo"
+	}
+
 	return "1"
 }
--- foobar/foobar_test.go
+++ foobar/foobar_test.go
@@ -3,10 +3,64 @@ package foobar_test
 import "testing"
 import "github.com/mesodiar/bello-go/foobar"
 
+func TestFooBarGiven2WantsString2(t *testing.T) {
+	given := 2
+	wants := "2"
+
+	get := foobar.Say(given)
+
+	if wants != get {
+		t.Errorf("given %d wants %q but got %q\n", given, wants, get)
+	}
+}
 
 func TestFooBarGiven1WantsString1(t *testing.T) {
 	given := 1
 	wants := "1"
+
+	get := foobar.Say(given)
+
+	if wants != get {
+		t.Errorf("given %d wants %q but got %q\n", given, wants, get)
+	}
+}
+
+func TestFooBarGiven4WantsString4(t *testing.T) {
+	given := 4
+	wants := "4"
+
+	get := foobar.Say(given)
+
+	if wants != get {
+		t.Errorf("given %d wants %q but got %q\n", given, wants, get)
+	}
+}
+
+func TestFooBarGiven5WantsStringBar(t *testing.T) {
+	given := 5
+	wants := "Bar"
+
+	get := foobar.Say(given)
+
+	if wants != get {
+		t.Errorf("given %d wants %q but got %q\n", given, wants, get)
+	}
+}
+
+func TestFooBarGiven6WantsStringFoo(t *testing.T) {
+	given := 6
+	wants := "Foo"
+
+	get := foobar.Say(given)
+
+	if wants != get {
+		t.Errorf("given %d wants %q but got %q\n", given, wants, get)
+	}
+}
+
+func TestFooBarGiven3WantsStringFoo(t *testing.T) {
+	given := 3
+	wants := "Foo"
 
 	get := foobar.Say(given)
 
