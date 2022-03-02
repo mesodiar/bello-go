e08e3ec732d94be37263b64d99bf1854d0730704
mesodiar <mils.meso@gmail.com>
2022-03-02 14:48:03 +0700

    "art" Foobar mod 3 :koala:

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -1,21 +1,16 @@
 package foobar
+import "strconv"
 
 var Title = "FooBar"
 var name = "Mils"
 
 func Say (n int) string {
-	if n == 2 {
-		return "2"
-	}
-	if n == 4 {
-		return "4"
-	}
 	if n == 5 {
 		return "Bar"
 	}
-	if n == 3 || n == 6 {
+	if n % 3 == 0 {
 		return "Foo"
 	}
 
-	return "1"
+	return strconv.Itoa(n)
 }
